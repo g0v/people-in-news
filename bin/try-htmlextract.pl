109adcfb60631503b49418c91bb4569977e84686
Kang-min Liu <gugod@gugod.org>
2019-03-03 09:08:20 +0900

    deal with character encoding on input!

--- bin/try-htmlextract.pl
+++ bin/try-htmlextract.pl
@@ -8,8 +8,10 @@ use Mojo::UserAgent;
 my $url = @ARGV[0] or die;
 
 my $tx = Sn::ua()->get($url);
+my $charset = Sn::tx_guess_charset($tx) or die "Failed to detect the charset";
 
-my $ex = Sn::HTMLExtractor->new( html => $tx->res->body );
+my $ex = Sn::HTMLExtractor->new( html => decode($charset, $tx->res->body) );
 say $ex->title;
 say "========";
+say $ex->dateline . "\n";
 say "\n" . $ex->content_text;
