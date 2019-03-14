280855e94e73527829372e8f65ca71eb0b374a5a
Kang-min Liu <gugod@gugod.org>
2019-03-14 09:35:33 +0900

    produce some output as early as possible.

--- bin/extract-presub.pl
+++ bin/extract-presub.pl
@@ -17,6 +17,7 @@ my %opts;
 GetOptions(
     \%opts,
     "db=s",
+    "threshold=n",
 );
 die "--db <DIR> is needed" unless -d $opts{db};
 
@@ -28,35 +29,30 @@ my $articles = Sn::ArticleIterator->new(
 my $count = 0;
 my %stats;
 my %extracted;
+my $threshold = $opts{threshold} || 42;      # an arbitrary choice.
 
 while (my $article = $articles->next) {
     my $text = $article->{content_text};
     my @stuff = split /\p{General_Category: Other_Punctuation}+/, $text;
     for my $phrase (@stuff) {
+        next unless length($phrase) > 2 && $phrase =~ /\A\p{Han}+\z/x;
+
         for my $len (2..5) {
-            my $re = '\p{Block: CJK}{' . $len . '}';
+            my $re = '\p{Han}{' . $len . '}';
             next unless length($phrase) > $len * 2 && $phrase =~ /\A($re) .+ ($re)\z/x;
             my ($prefix, $suffix) = ($1, $2);
             $stats{prefix}{$prefix}++ unless $extracted{$prefix};
             $stats{suffix}{$suffix}++ unless $extracted{$suffix};
+
+            for my $x ($prefix, $suffix) {
+                if (! $extracted{$x} && $stats{prefix}{$x} && $stats{suffix}{$x} && $stats{prefix}{$x} > $threshold && $stats{suffix}{$x} > $threshold) {
+                    $extracted{$prefix} = 1;
+                    delete $stats{prefix}{$x};
+                    delete $stats{suffix}{$x};
+
+                    say encode_utf8($x);
+                }
+            }
         }
-    }
-
-    if ($count++ > 9999) {
-        my $threshold = 9;      # an arbitrary choice.
-        for my $x (keys %{$stats{prefix}}) {
-            next unless $stats{prefix}{$x} > $threshold;
-            next unless $stats{suffix}{$x} && $stats{suffix}{$x} > $threshold;
-            $extracted{$x} = 1;
-
-            delete $stats{prefix}{$x};
-            delete $stats{suffix}{$x};
-
-            say encode_utf8($x);
-        }
-        $count = 0;
     }
 }
-
-
-
