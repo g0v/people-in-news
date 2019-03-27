8576d1179d2f9ccbd71235cc9b8a9114cfd8bb46
Kang-min Liu <gugod@gugod.org>
2019-03-27 09:59:30 +0900

    a few more newspaper names

--- lib/Sn/Constants.pm
+++ lib/Sn/Constants.pm
@@ -12,6 +12,12 @@ our @EXPORT = ('%SNRE', get_public_functions());
 
 use constant {
     NEWSPAPER_NAMES => [
+        '芋傳媒 TaroNews',
+        '上報',
+        '中央社',
+        '蕃新聞',
+        '無綫新聞',
+        '蘋果日報',
         '自由時報電子報',
         '中國時報',
         '工商時報',
