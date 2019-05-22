b5bfe00a5d1c718ac8764a6567dae597e21b16d8
Kang-min Liu <gugod@gugod.org>
2019-05-23 07:34:45 +0900

    add a few more NEWSPAPER_NAMES

--- lib/Sn/Constants.pm
+++ lib/Sn/Constants.pm
@@ -12,21 +12,27 @@ our @EXPORT = ('%SNRE', get_public_functions());
 
 use constant {
     NEWSPAPER_NAMES => [
-        '鉅亨網',
-        '新頭殼 Newtalk',
+        'PChome 新聞',
+        '一零一傳媒',
         '上報',
         '中國時報',
         '中央社 CNA',
         '中央社',
+        '中華日報新聞網',
+        '台灣好新聞 TaiwanHot.net',
         '工商時報',
+        '新頭殼 Newtalk',
         '旺報',
         '無綫新聞',
+        '聯合新聞網',
         '自由娛樂',
         '自由時報電子報',
         '芋傳媒 TaroNews',
         '蕃新聞',
         '蘋果新聞網｜蘋果日報',
         '蘋果日報',
+        '鉅亨網',
+        'NOWnews 今日新聞',
     ],
 
     CATEGORY_NAMES => [
