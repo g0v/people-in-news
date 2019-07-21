e2599a9aa52c0235fff6156a3b6f66d3ae565f21
Kang-min Liu <gugod@gugod.org>
2019-07-21 13:32:38 +0900

    adjust so that each process_* subroutines process only url

--- bin/gather.pl
+++ bin/gather.pl
@@ -15,7 +15,7 @@ use Getopt::Long qw(GetOptions);
 use Algorithm::BloomFilter;
 
 use List::Util qw(uniqstr max shuffle);
-use JSON::PP qw(encode_json);
+use JSON ();
 use FindBin '$Bin';
 
 use Sn;
@@ -25,6 +25,8 @@ use Sn::HTMLExtractor;
 use Sn::ArticleExtractor;
 use Sn::FTVScraper;
 
+use constant CUTOFF => $ENV{CUTOFF} || 999;
+
 ## global
 my $PROCESS_START = time();
 my $STOP = 0;
@@ -32,6 +34,11 @@ local $SIG{INT} = sub { $STOP = 1 };
 
 sub err {
     say STDERR @_;
+}
+
+sub encode_article_as_json {
+    state $json = JSON->new->utf8->canonical;
+    $json->encode($_[0]);
 }
 
 sub looks_like_similar_host {
@@ -47,8 +54,10 @@ sub looks_like_xml {
     return $url =~ m{ \com/tag/(?:.+)\.xml \z};
 }
 
-sub process {
-    my ($urls, $url_seen, $out) = @_;
+sub process_generic {
+    my ($url, $url_seen, $out) = @_;
+
+    say "[$$] Process generically: $url";
 
     open my $fh, '>', $out;
     $fh->autoflush(1);
@@ -56,7 +65,7 @@ sub process {
     my $error_count = 0;
     my $extracted_count = 0;
 
-    my @links = @$urls;
+    my @links = ($url);
     my %seen = map { $_ => 1 } @links;
 
     my $round = 0;
@@ -84,7 +93,7 @@ sub process {
                     delete $article->{links};
 
                     if ($article->{title}) {
-                        my $line = encode_json($article) . "\n";
+                        my $line = encode_article_as_json($article) . "\n";
                         print $fh $line;
                         $extracted_count++;
                         push @processed_links, $url;
@@ -92,7 +101,7 @@ sub process {
                 } else {
                     say "[$$] Fail to extract from $url";
                 }
-                return ($STOP || ($extracted_count > 999)) ? 0 : 1;
+                return ($STOP || ($extracted_count > CUTOFF)) ? 0 : 1;
             },
             sub {
                 my ($error, $url) = @_;
@@ -114,6 +123,8 @@ sub process {
 sub process_ftv {
     my ($url_seen, $out) = @_;
 
+    say "[$$] Process specially: ftv";
+
     open my $fh, '>', $out;
     $fh->autoflush(1);
 
@@ -126,11 +137,11 @@ sub process_ftv {
 
         my $article = Sn::FTVScraper->scrape($url) or next;
         if ($article->{title}) {
-            my $line = encode_json($article) . "\n";
+            my $line = encode_article_as_json($article) . "\n";
             print $fh $line;
             push @processed_links, $url;
         }
-        last if @processed_links > 999;
+        last if @processed_links > CUTOFF;
     }
     if (@processed_links) {
         add_to_url_seen(\@processed_links);
@@ -143,6 +154,7 @@ my $url_seen;
 sub add_to_url_seen {
     my ($urls) = @_;
     $url_seen->add(@$urls);
+    say "Added " . (0+ @$urls) . " more urls";
 }
 
 # main
@@ -163,37 +175,31 @@ my @initial_urls;
 if (@ARGV) {
     @initial_urls = @ARGV;
 } else {
-    @initial_urls = (
+    @initial_urls =  shuffle(
         @{ Sn::read_string_list('etc/news-sites.txt') },
         @{ Sn::read_string_list('etc/news-aggregation-sites.txt') },
     );
 }
 
-my @special = grep { /ftv\.com|ftvnews\.com/ } @initial_urls;
-@initial_urls = grep { ! /ftv\.com|ftvnews\.com/ } @initial_urls;
-
 # jsonl => http://jsonlines.org/
-
-if (@special) {
-    my $output = $opts{db} . "/articles-". Sn::ts_now() .".jsonl";
-    while (-f $output) {
-        sleep 1;
-        $output = $opts{db} . "/articles-". Sn::ts_now() .".jsonl";
-    }
-    process_ftv($url_seen, $output);
-}
-
 if (@initial_urls) {
-    MCE::Loop::init { chunk_size => 'auto' };
+    MCE::Loop::init { chunk_size => 1 };
     mce_loop {
+        my $url = $_;
+
         my $output = $opts{db} . "/articles-". Sn::ts_now() .".jsonl";
         while (-f $output) {
             sleep 1;
             $output = $opts{db} . "/articles-". Sn::ts_now() .".jsonl";
         }
         $STOP = 1 if time() - $PROCESS_START > 3000;
-        process($_, $url_seen, $output);
-    } shuffle @initial_urls;
+
+        if ($url =~ /ftv\.com|ftvnews\.com/) {
+            process_ftv($url_seen, $output);
+        } else {
+            process_generic($url, $url_seen, $output);
+        }
+    } @initial_urls;
 }
 
 $url_seen->save;
