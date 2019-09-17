f263111eb28c2aae0d82e176e72ef12b8365e4f8
Kang-min Liu <gugod@gugod.org>
2019-09-18 07:49:46 +0900

    move a few processes that was done externally to be here.

--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,8 @@
 prog=$0
 cd $(dirname $0)/../
 
+export TZ=Asia/Taipei
+
 ts_begin=$(date +%s)
 
 perl -Ilib bin/gather-feeds.pl --db var/db
@@ -18,6 +20,11 @@ perl -Ilib bin/atom2rss.pl var/www/articles-summarized.atom var/www/articles-sum
 
 perl -Ilib bin/build-dailystats.pl --db var/db -o var/db
 perl -Ilib bin/emit-hourly-stats.pl --db var/db
+
+for f in $(ls -1 var/db/*.jsonl | grep -v $(date +%Y%m%d))
+do
+    pigz -11 $f
+done
 
 ts_end=$(date +%s)
 
