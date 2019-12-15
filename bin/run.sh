7c710cae7d9c3cf0e5bf55e85e5ecd39ee66812e
Kang-min Liu <gugod@gugod.org>
2019-12-15 11:14:21 +0900

    Stop doing those because the are basically un-used.

--- bin/run.sh
+++ bin/run.sh
@@ -9,9 +9,7 @@ ts_begin=$(date +%s)
 
 perl -Ilib bin/gather-feeds.pl --db var/db
 perl -Ilib bin/gather.pl --db var/db
-perl -Ilib bin/build-daily-md.pl --db var/db -o var/www
-perl -Ilib bin/build-www.pl -i var/www -o var/www
-perl -Ilib bin/merge.pl --db var/db 
+perl -Ilib bin/merge.pl --db var/db
 
 perl -Ilib bin/build-atom-feed.pl --db var/db -o var/www
 perl -Ilib bin/atom2rss.pl var/www/articles-full.atom var/www/articles-full.rss
