49e10d693dbb0c32429ff839c987c3ff22fc84cb
Kang-min Liu <gugod@gugod.org>
2019-12-20 21:25:00 +0900

    a program to merge many *.jsonl.gz into a yearly archive.

---
+++ bin/merge-yearly.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+use v5.26;
+use warnings;
+use Getopt::Long qw(GetOptions);
+use Data::Dumper qw(Dumper);
+use PerlIO::via::gzip;
+
+## main
+my %opts;
+GetOptions(
+    \%opts,
+    "force|f",
+    "db|d=s",
+);
+die "--db <DIR> is needed" unless  $opts{db} && -d $opts{db};
+
+my $this_yyyy = (localtime())[5] + 1900;
+
+for my $table (qw(articles)) {
+    my %buckets;
+    for my $file (glob "$opts{db}/${table}-*.jsonl.gz") {
+        my ($k) = $file =~ m/ - ([0-9]{4})[0-9]+\.jsonl\.gz \z/x;
+        next unless $k && $k ne $this_yyyy;
+        push @{$buckets{$k}}, $file;
+    }
+
+    for my $yyyy (keys %buckets) {
+        my $output = $opts{db} . "/${table}-$yyyy.jsonl.gz";
+        next if -f $output;
+
+        my $output_part = $output . ".part";
+        open my $fh, '>:via(gzip)', $output_part;
+
+        for my $input (@{$buckets{$yyyy}}) {
+            open my $infh, '<:via(gzip)', $input;
+            while (<$infh>) {
+                print $fh $_;
+            }
+            close($infh);
+        }
+        close($fh);
+
+        unlink(@{$buckets{$yyyy}});
+        link($output_part, $output);
+        unlink($output_part);
+    }
+}
