abe4b4cf466cf5f13e46ffdeba07d9c7bec229e4
Kang-min Liu <gugod@gugod.org>
2020-05-06 23:11:17 +0800

    Not really reading those daily stats.
    Also, move the .jsonl.gz int yearly partition.

--- bin/run.sh
+++ bin/run.sh
@@ -10,17 +10,19 @@ ts_begin=$(date +%s)
 perl -Ilib bin/gather-feeds.pl --db var/db
 perl -Ilib bin/gather.pl --db var/db --time-limit=2400
 perl -Ilib bin/merge.pl --db var/db
-
 perl -Ilib bin/deduplicate.pl --yes var/db/*.jsonl
-
 perl -Ilib bin/build-atom-feed.pl --db var/db -o var/www
-
-perl -Ilib bin/build-dailystats.pl --db var/db -o var/db
 perl -Ilib bin/emit-hourly-stats.pl --db var/db
 
 for f in $(ls -1 var/db/*.jsonl | grep -v $(date +%Y%m%d))
 do
     pigz -11 $f
+done
+
+for yyyy in 2020 2021
+do
+    mkdir -p var/db/$yyyy/
+    mv var/db/*$yyyy*.jsonl.gz var/db/$yyyy/
 done
 
 ts_end=$(date +%s)
