6a3b7641aa31b45abe65bff7d9dff6073ee272e7
Kang-min Liu <gugod@gugod.org>
2020-08-22 10:12:54 +0900

    Add 'map' method to Iterator

--- lib/Sn/Iterator.pm
+++ lib/Sn/Iterator.pm
@@ -14,6 +14,11 @@ has reified => (
     default => sub { [ ] }
 );
 
+has mapper => (
+    is => "rw",
+    predicate => 1,
+);
+
 use overload '&{}' => sub {
     my $self = $_[0];
     return sub { $self->next() }
@@ -23,7 +28,14 @@ sub next {
     my ($self) = @_;
 
     if ( !$self->_has_cursor || $self->_cursor == @{$self->reified}) {
-        $self->reified( $self->reify );
+        my $items = $self->reify;
+
+        if ($self->has_mapper) {
+            my $code = $self->mapper;
+            @$items = map { $code->($_) } @$items;
+        }
+
+        $self->reified($items);
         $self->_cursor(0);
     }
 
@@ -39,6 +51,14 @@ sub exhaust {
         push @ret, $o;
     }
     return \@ret;
+}
+
+sub map {
+    my ($self, $mapper) = @_;
+    my $class = ref($self);
+    my $new_iter = bless +{ %$self }, $class;
+    $new_iter->mapper($mapper);
+    return $new_iter;
 }
 
 1;
--- t/Sn-filter-iterator.t
+++ t/Sn-filter-iterator.t
@@ -15,6 +15,17 @@ subtest 'simple IntIterator' => sub {
     is($numbers, [1,2,3,4,5,6,7,8,9]);
 };
 
+subtest 'IntIterator with map' => sub {
+    my $iter = Sn::IntIterator->new(
+        from  => 1,
+        until => 10,
+    )->map(sub { $_ * $_ });
+
+    my $numbers = $iter->exhaust;
+    is($numbers, [1,4,9,16,25,36,49,64,81]);
+};
+
+
 subtest 'Only Odd numbers' => sub {
     my $iter1 = Sn::IntIterator->new(
         from  => 1,
